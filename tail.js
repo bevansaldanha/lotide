b032452db7b8f2cddcbe2c0a32274b5686ac4817
Bevan Saldanha <bevansaldanha15@gmail.com>
2023-09-18 19:37:28 -0400

    Added tail.js

---
+++ tail.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅  Assertion Passed: ${actual} ===  ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
+  }
+};
+const tail = function(array) {
+  return array.slice(1);
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result[1], "Lighthouse");
