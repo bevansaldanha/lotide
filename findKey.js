1e0f2865e2eadcb4988cdf3c54ff557794c2c340
Bevan Saldanha <bevansaldanha15@gmail.com>
2023-09-21 19:22:33 -0400

    Created findKey.js

---
+++ findKey.js
@@ -0,0 +1,42 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅  Assertion Passed: ${actual} ===  ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
+  }
+};
+
+const findKey  = function(obj,callback) {
+  for (let keys in obj) {
+    if (callback(obj[keys])) {
+      return  keys;
+    }
+  } return undefined;
+};
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2))
+
+console.log(findKey({
+  "Blue Hill": { stars: 1, chefs: 12 },
+  "Akaleri":   { stars: 3, chefs: 21 },
+  "noma":      { stars: 2, chefs: 24 },
+  "elBulli":   { stars: 3, chefs: 21},
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.chefs === 21));
+
+console.log(findKey({
+  "Blue Hill": { stars: 1, chefs: 12 },
+  "Akaleri":   { stars: 3, chefs: 21 },
+  "noma":      { stars: 2, chefs: 24 },
+  "elBulli":   { stars: 3, chefs: 21},
+  "Ora":       { stars: 2, service: "idk, maybe?"},
+  "Akelarre":  { stars: 3, service: "nah, not today"}
+}, x => x.service === 'yes'));
