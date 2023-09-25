5fe8665a273d0c8ef1b1d9f41cc005f5b1aa868a
Bevan Saldanha <bevansaldanha15@gmail.com>
2023-09-25 14:28:48 -0400

    Refactored middle.js

--- middle.js
+++ middle.js
@@ -13,10 +13,8 @@ const middle = function(arr) {
   return midArray;
 };
 
-assertArraysEqual(middle(['q', 2, 3, 4, 5, 4]),middle([1, 2, 3, 4, 5, 6]));
-assertArraysEqual(middle(['r', 2, 3, 5, 5, 9]),middle([1, 2, 3, 4, 5, 6]));
-assertArraysEqual(middle([100,'q', 2, 3, 4, 5, 4, 'w']),middle([1, 2, 3, 4, 5, 6]));
-assertArraysEqual(middle([ 3, 4, 5, 4]),middle([1, 2, 3, 4, 5, 6]));
-assertArraysEqual(middle(['q', 2, 3, 4, 5, 4, true]),middle([false, 1, 2, 3, 4, 5, 6]));
+module.exports = middle;
+
+
 
 
