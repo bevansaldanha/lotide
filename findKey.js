0bbaa7ebc8b2d8db04968909a2b8ae2fdacc0c29
Bevan Saldanha <bevansaldanha15@gmail.com>
2023-09-25 16:14:32 -0400

    updated files to get them to work and shorted syntax for index

--- findKey.js
+++ findKey.js
@@ -7,30 +7,3 @@ const findKey  = function(obj,callback) {
 };
 
 module.exports = findKey;
-
-console.log(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2))
-
-console.log(findKey({
-  "Blue Hill": { stars: 1, chefs: 12 },
-  "Akaleri":   { stars: 3, chefs: 21 },
-  "noma":      { stars: 2, chefs: 24 },
-  "elBulli":   { stars: 3, chefs: 21},
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.chefs === 21));
-
-console.log(findKey({
-  "Blue Hill": { stars: 1, chefs: 12 },
-  "Akaleri":   { stars: 3, chefs: 21 },
-  "noma":      { stars: 2, chefs: 24 },
-  "elBulli":   { stars: 3, chefs: 21},
-  "Ora":       { stars: 2, service: "idk, maybe?"},
-  "Akelarre":  { stars: 3, service: "nah, not today"}
-}, x => x.service === 'yes'));
--- findKeyByValue.js
+++ findKeyByValue.js
@@ -9,13 +9,4 @@ const findKeyByValue = function(obj, val) {
 
 module.exports = findKeyByValue;
 
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
 
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "drama");
