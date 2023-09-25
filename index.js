76b32f92f5b933e357be6988860a7f87e2f01d12
Bevan Saldanha <bevansaldanha15@gmail.com>
2023-09-25 15:48:31 -0400

    added countLetters

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const head = require('./head');
 const tail = require('./tail');
 const middle = require('./middle');
 const countOnly = require('./countOnly');
+const countLetters = require('./countLetters');
 const findKey = require('./findKey');
 const findKeyByValue = require("./findKeyByValue");
 const flatten = require('./flatten');
@@ -17,6 +18,7 @@ module.exports = {
   tail: tail,
   middle: middle,
   countOnly: countOnly,
+  countLetters: countLetters,
   findKey: findKey,
   findKeyByValue: findKeyByValue,
   flatten: flatten,
